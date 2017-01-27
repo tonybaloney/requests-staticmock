bb5494dff6c2e398dc5c634d6adc2fc950fb8977
Anthony Shaw <anthonyshaw@apache.org>
2017-01-27 16:28:28 +1100

    add context manager method

---
+++ requests_staticmock/context.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+# Licensed to Anthony Shaw (anthonyshaw@apache.org) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import contextlib
+
+from requests.compat import OrderedDict
+from requests_staticmock import adapter
+
+
+@contextlib.contextmanager
+def mock_session_with_fixtures(session, path, url):
+    """
+    Context Manager
+
+    Mock the responses with a particular session
+    to any files found within a static path
+
+    :param session: The requests session object
+    :type  session: :class:`requests.Session`
+
+    :param path: The path to the fixtures
+    :type  path: ``str``
+
+    :param url: The base URL to mock, e.g. http://mock.com, http://
+        supports a single URL or a list
+    :type  url: ``str`` or ``list``
+    """
+    _orig_adapters = session.adapters
+    mock_adapter = adapter.Adapter()
+    session.adapters = OrderedDict()
+    if isinstance(url, (list, tuple)):
+        for u in url:
+            session.mount(u, mock_adapter)
+    else:
+        session.mount(url, mock_adapter)
+    mock_adapter.register_path(path)
+    yield
+    session.adapters = _orig_adapters
--- tests/test_requests_staticmock.py
+++ tests/test_requests_staticmock.py
@@ -20,14 +20,15 @@ except ImportError:
     import unittest
 
 from requests.compat import OrderedDict
-from requests_staticmock import adapter
+from requests_staticmock import (Adapter,
+                                 mock_session_with_fixtures)
 from requests import Session
 
 
 class TestRequestsStaticMock(unittest.TestCase):
     def setUp(self):
         self.session = Session()
-        a = adapter.Adapter()
+        a = Adapter()
         self.session.adapters = OrderedDict()
         self.session.mount("http://test.com", a)
         a.register_path('tests/fixtures')
@@ -48,6 +49,15 @@ class TestRequestsStaticMock(unittest.TestCase):
         with self.assertRaises(Exception):
             response.raise_for_status()
         self.assertEquals(response.status_code, 404)
+
+    def test_context_manager(self):
+        new_session = Session()
+        with mock_session_with_fixtures(new_session, 'tests/fixtures',
+                                        'http://test_context.com'):
+            response = new_session.request('get', 'http://test_context.com/test.txt')
+            self.assertEqual(response.text, 'Hello world!')
+        # assert resets back to default 2 adapters
+        self.assertEqual(len(new_session.adapters), 2)
 
 if __name__ == '__main__':
     import sys
