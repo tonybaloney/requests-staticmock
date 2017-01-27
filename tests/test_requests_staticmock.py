5e5df6ac56bc72e2ca4888eb6356b1414e3b05dc
Anthony Shaw <anthonyshaw@apache.org>
2017-01-28 09:38:22 +1100

    complete migration to pytest

--- tests/test_requests_staticmock.py
+++ tests/test_requests_staticmock.py
@@ -14,10 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-try:
-    import unittest2 as unittest
-except ImportError:
-    import unittest
+import pytest
 
 from requests.compat import OrderedDict
 from requests_staticmock import (Adapter,
@@ -28,79 +25,79 @@ from requests_staticmock import (Adapter,
 from requests import Session
 
 
-class TestRequestsStaticMock(unittest.TestCase):
-    def setUp(self):
-        self.session = Session()
-        a = Adapter()
-        self.session.adapters = OrderedDict()
-        self.session.mount("http://test.com", a)
-        a.register_path('tests/fixtures')
+def _get_session():
+    session = Session()
+    a = Adapter()
+    session.adapters = OrderedDict()
+    session.mount("http://test.com", a)
+    a.register_path('tests/fixtures')
+    return session
 
-    def tearDown(self):
-        pass
 
-    def test_session_adapter(self):
-        response = self.session.request('get', 'http://test.com/test.txt')
-        self.assertEqual(response.text, 'Hello world!')
+def test_session_adapter():
+    response = _get_session().request('get', 'http://test.com/test.txt')
+    assert response.text == 'Hello world!'
 
-    def test_json_responses(self):
-        response = self.session.request('get', 'http://test.com/test_json.json')
-        self.assertEqual(response.json()['hello'], 'world')
 
-    def test_bad_response(self):
-        response = self.session.request('get', 'http://test.com/bad.url')
-        with self.assertRaises(Exception):
-            response.raise_for_status()
-        self.assertEquals(response.status_code, 404)
+def test_json_responses():
+    response = _get_session().request('get', 'http://test.com/test_json.json')
+    assert response.json()['hello'] == 'world'
 
-    def test_context_manager(self):
-        new_session = Session()
-        with mock_session_with_fixtures(new_session, 'tests/fixtures',
-                                        'http://test_context.com'):
-            response = new_session.request('get', 'http://test_context.com/test.txt')
-            self.assertEqual(response.text, 'Hello world!')
-        # assert resets back to default 2 adapters
-        self.assertEqual(len(new_session.adapters), 2)
 
-    def test_class_adapter(self):
-        class_session = Session()
-        class TestMockClass(BaseMockClass):
-            def _test_json(self, request):
-                return "hello alabama"
+def test_bad_response():
+    response = _get_session().request('get', 'http://test.com/bad.url')
+    with pytest.raises(Exception):
+        response.raise_for_status()
+    assert response.status_code == 404
 
-        a = ClassAdapter(TestMockClass)
-        class_session.adapters = OrderedDict()
-        class_session.mount("http://test.com", a)
+
+def test_context_manager():
+    new_session = Session()
+    with mock_session_with_fixtures(new_session, 'tests/fixtures',
+                                    'http://test_context.com'):
+        response = new_session.request('get', 'http://test_context.com/test.txt')
+        assert response.text == 'Hello world!'
+    # assert resets back to default 2 adapters
+    assert len(new_session.adapters) == 2
+
+
+def test_class_adapter():
+    class_session = Session()
+    class TestMockClass(BaseMockClass):
+        def _test_json(self, request):
+            return "hello alabama"
+
+    a = ClassAdapter(TestMockClass)
+    class_session.adapters = OrderedDict()
+    class_session.mount("http://test.com", a)
+    response = class_session.get('http://test.com/test.json')
+    assert response.text == "hello alabama"
+
+
+def test_class_context_manager():
+    class_session = Session()
+    class TestMockClass(BaseMockClass):
+        def _test_json(self, request):
+            return "hello alabama"
+
+    a = ClassAdapter(TestMockClass)
+    with mock_session_with_class(class_session, TestMockClass, 'http://test.com'):
         response = class_session.get('http://test.com/test.json')
-        self.assertEquals(response.text, "hello alabama")
+    assert response.text == "hello alabama"
 
-    def test_class_context_manager(self):
-        class_session = Session()
-        class TestMockClass(BaseMockClass):
-            def _test_json(self, request):
-                return "hello alabama"
 
-        a = ClassAdapter(TestMockClass)
-        with mock_session_with_class(class_session, TestMockClass, 'http://test.com'):
-            response = class_session.get('http://test.com/test.json')
-        self.assertEquals(response.text, "hello alabama")
+def test_class_context_manager_with_params():
+    class_session = Session()
+    class TestMockClass(BaseMockClass):
+        def _test_json(self, request):
+            if request.method == 'GET':
+                return 'detroit'
+            else:
+                return 'san diego'
 
-    def test_class_context_manager_with_params(self):
-        class_session = Session()
-        class TestMockClass(BaseMockClass):
-            def _test_json(self, request):
-                if request.method == 'GET':
-                    return 'detroit'
-                else:
-                    return 'san diego'
-
-        a = ClassAdapter(TestMockClass)
-        with mock_session_with_class(class_session, TestMockClass, 'http://test.com'):
-            response1 = class_session.get('http://test.com/test.json')
-            response2 = class_session.post('http://test.com/test.json', data='123')
-        self.assertEquals(response1.text, 'detroit')
-        self.assertEquals(response2.text, 'san diego')
-
-if __name__ == '__main__':
-    import sys
-    sys.exit(unittest.main())
+    a = ClassAdapter(TestMockClass)
+    with mock_session_with_class(class_session, TestMockClass, 'http://test.com'):
+        response1 = class_session.get('http://test.com/test.json')
+        response2 = class_session.post('http://test.com/test.json', data='123')
+    assert response1.text == 'detroit'
+    assert response2.text == 'san diego'
