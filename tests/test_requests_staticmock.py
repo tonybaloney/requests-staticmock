013068801d7b249e47e2d1b87d2a0a7217131249
Anthony Shaw <anthonyshaw@apache.org>
2017-05-10 13:44:46 +1000

    linting

--- tests/test_requests_staticmock.py
+++ tests/test_requests_staticmock.py
@@ -240,5 +240,7 @@ def test_stream_request():
 
 
 def test_query_params():
-    response = _get_session().request('get', 'http://test.com/test2.txt', params={'query': 'test'})
+    response = _get_session().request(
+        'get', 'http://test.com/test2.txt', 
+        params={'query': 'test'})
     assert response.text == 'test'
