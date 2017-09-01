661914acdd86e7091838bc82b945901e6b41a0e7
Anthony Shaw <anthonyshaw@apache.org>
2017-09-01 14:47:58 +1000

    Bump version: 1.2.0 → 1.3.0

--- requests_staticmock/__init__.py
+++ requests_staticmock/__init__.py
@@ -21,4 +21,4 @@ from requests_staticmock.abstractions import BaseMockClass  # NOQA
 
 __author__ = 'Anthony Shaw'
 __email__ = 'anthonyshaw@apache.org'
-__version__ = '1.2.0'
+__version__ = '1.3.0'
--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='requests-staticmock',
-    version='1.2.0',
+    version='1.3.0',
     description="A static HTTP mock interface for requests",
     long_description=readme + '\n\n' + history,
     author="Anthony Shaw",
